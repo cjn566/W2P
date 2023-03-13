9e74df3ef4bb1336ade69380e0f0e92a13dd9643
Colten Nye <cjn566@gmail.com>
2023-03-13 16:34:54 -0700

    bug fix 2

--- server/api/hello.js
+++ server/api/hello.js
@@ -1,6 +1,6 @@
 import query from '../db'
 export default defineEventHandler(async (event) => {
-    const res = await query('SELECT * FROM app.games WHERE entry_id = $1', [1]).rows[0]
+    const res = (await query('SELECT * FROM app.games WHERE entry_id = $1', [1])).rows[0]
     return {
       game_user: res.user_id
     }
